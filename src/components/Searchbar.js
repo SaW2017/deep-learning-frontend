8c588e7c30604eccf0fcf8c32b06f685397b1452
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-07-18 23:47:48 +0200

    Added functionality for changing the filter value with clicking on a button

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,35 +1,48 @@
-import {Typography} from "@material-ui/core";
+import {Typography, Button} from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from "@material-ui/core/styles";
+import {useState} from "react";
 
 const searchbarStyle = makeStyles({
     searchbar: {
         width: "inherit",
         padding: "20px"
     },
-    input:{
+    input: {
         marginTop: "15px"
+    },
+    button: {
+        width: "inherit",
+        padding: "20px"
     }
 });
 
 const Searchbar = (props) => {
     const classes = searchbarStyle();
+    const [conceptValue, setConceptValue] = useState("");
 
     return (
         <div className={classes.searchbar}>
             <Typography>Concept:</Typography>
             <TextField id="outlined-search"
                        className={classes.input}
-                       label="Search field"
+                       label="Enter Concept"
                        type="search"
                        variant="outlined"
+                       value={conceptValue}
                        onChange={event => {
-                            props.setFilter((event.target.value));
-                            console.log("Searchbar");
-                            console.log(event.target.value);
-                        }}
+                           var eventValue = event.target.value;
+                           console.log("Searchbar");
+                           console.log(eventValue);
+                           setConceptValue(eventValue);
+                       }}
             />
+            <Button type={"submit"} color={"primary"} onClick={() => {
+                props.setFilter(conceptValue);
+            }
+            }>Search</Button>
         </div>
+
     );
 }
 
